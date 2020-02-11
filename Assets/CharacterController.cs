db553b3e61e05f9ef868a0e1fef9ac77d10f3f2b
Wiktor Mendalka <wicia_6@wp.pl>
2020-02-11 18:03:27 +0100

    File upload

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private static readonly int TriggerId = Animator.StringToHash("Next");
+
+    public void ChangeAnimation()
+    {
+        this.GetComponent<Animator>().SetTrigger(TriggerId);
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] characters;
+
+    public void SetCharacter(int c)
+    {
+        for (var i = 0; i < this.characters.Length; i++)
+        {
+            this.characters[i].SetActive(i == c);
+        }
+    }
+
+    public void ChangeAnimation()
+    {
+        foreach (var controller in this.characters.Select(c => c.GetComponent<CharacterController>()))
+        {
+            controller.ChangeAnimation();
+        }
+    }
+}
